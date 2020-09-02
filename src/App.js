581d99c4b2cf496ae2975cef8c7e49cade6a923b
Diptiranjan Sukla <sukla.dipti@gmail.com>
2020-09-02 20:30:07 +0530

    added multi filter

--- src/App.js
+++ src/App.js
@@ -19,13 +19,19 @@ const lunchYear = [
 
 const App = () => {
   const [flightData, setFlightData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [launch_year, setLaunchYear] = useState("");
+  const [susslaunch, setSussLaunch] = useState("");
+  const [susslanding, setSusslanding] = useState("");
 
   useEffect(() => {
+    setLoading(true);
     function spacex() {
       fetch(`https://api.spacexdata.com/v3/launches?limit=100`)
         .then(resp => resp.json())
         .then(function(data) {
           setFlightData(data);
+          setLoading(false);
         });
     }
     spacex();
@@ -33,12 +39,15 @@ const App = () => {
 
   //filter year
   const launchYear = param => () => {
-    fetch(
-      `https://api.spacexdata.com/v3/launches?limit=100&launch_year=${param}`
-    )
+    setLaunchYear(param);
+    const query_str = `&launch_year=${param}&land_success=${susslanding}&launch_success=${susslaunch}`;
+
+    setLoading(true);
+    fetch(`https://api.spacexdata.com/v3/launches?limit=100${query_str}`)
       .then(resp => resp.json())
       .then(function(data) {
         setFlightData(data);
+        setLoading(false);
       });
   };
   useEffect(() => {
@@ -47,13 +56,14 @@ const App = () => {
 
   // filter launchSuccess true/false
   const launchSuccess = param => e => {
-    fetch(
-      `https://api.spacexdata.com/v3/launches?limit=100&launch_success=${param}`
-    )
+    setSussLaunch(param);
+    const query_str = `&launch_year=${launch_year}&land_success=${susslanding}&launch_success=${param}`;
+    setLoading(true);
+    fetch(`https://api.spacexdata.com/v3/launches?limit=100${query_str}`)
       .then(resp => resp.json())
       .then(function(data) {
-        console.log("dipranjan", data);
         setFlightData(data);
+        setLoading(false);
       });
   };
   useEffect(() => {
@@ -62,13 +72,15 @@ const App = () => {
 
   // filter change landing true/false
   const changeLading = param1 => e => {
-    fetch(
-      `https://api.spacexdata.com/v3/launches?limit=100&land_success=${param1}`
-    )
+    setSusslanding(param1);
+    const query_str = `&launch_year=${launch_year}&land_success=${param1}&launch_success=${susslaunch}`;
+
+    setLoading(true);
+    fetch(`https://api.spacexdata.com/v3/launches?limit=100${query_str}`)
       .then(resp => resp.json())
       .then(function(data) {
-        console.log("dipranjan1", data);
         setFlightData(data);
+        setLoading(false);
       });
   };
   useEffect(() => {
@@ -88,7 +100,11 @@ const App = () => {
           <ul>
             {lunchYear.map(res => {
               return (
-                <li key={res} onClick={launchYear(res)}>
+                <li
+                  key={res}
+                  onClick={launchYear(res)}
+                  className={launch_year === res ? "highlited_color" : ""}
+                >
                   {res}
                 </li>
               );
@@ -98,58 +114,82 @@ const App = () => {
           <div style={{ textAlign: "center" }}>Successful Launch</div>
           <hr />
           <ul>
-            <li onClick={launchSuccess("true")}>True</li>
-            <li onClick={launchSuccess("false")}>False</li>
+            <li
+              onClick={launchSuccess("true")}
+              className={susslaunch === "true" ? "highlited_color" : ""}
+            >
+              True
+            </li>
+            <li
+              onClick={launchSuccess("false")}
+              className={susslaunch === "false" ? "highlited_color" : ""}
+            >
+              False
+            </li>
           </ul>
           <div style={{ clear: "both" }}></div>
           <div style={{ textAlign: "center" }}>Successful Landing</div>
           <hr />
           <ul>
-            <li onClick={changeLading("true")}>True</li>
-            <li onClick={changeLading("false")}>False</li>
+            <li
+              onClick={changeLading("true")}
+              className={susslanding === "true" ? "highlited_color" : ""}
+            >
+              True
+            </li>
+            <li
+              onClick={changeLading("false")}
+              className={susslanding === "false" ? "highlited_color" : ""}
+            >
+              False
+            </li>
           </ul>
         </div>
 
         <div className="col-9">
           <div className="row">
-            {flightData.map(res => {
-              const {
-                links,
-                mission_name,
-                flight_number,
-                mission_id,
-                launch_year,
-                launch_success,
-                rocket: { first_stage }
-              } = res;
-              return (
-                <div className="col-3 card" key={flight_number}>
-                  <img
-                    src={links.mission_patch_small}
-                    alt="Avatar"
-                    style={{ width: "100%" }}
-                  />
-                  <div className="container">
-                    <h4>
-                      <strong>{mission_name}</strong> #{" "}
-                      <strong>{flight_number}</strong>
-                    </h4>
-                    <h5>MissionIds :{mission_id}</h5>
-                    <h5>Launch Year: {launch_year}</h5>
-                    <h5>
-                      Successful Lunch:{" "}
-                      {launch_success === true ? "true" : "false"}
-                    </h5>
-                    <h5>
-                      Successful Landing:{" "}
-                      {first_stage.cores[0].land_success === true
-                        ? "true"
-                        : "false"}
-                    </h5>
+            {loading ? <h3>Loading....</h3> : ""}
+            {!loading && flightData.length == 0 && <h4>No Records found!</h4>}
+            {flightData.length > 0 &&
+              !loading &&
+              flightData.map((res, index) => {
+                const {
+                  links,
+                  mission_name,
+                  flight_number,
+                  mission_id,
+                  launch_year,
+                  launch_success,
+                  rocket: { first_stage }
+                } = res;
+                return (
+                  <div className="col-3 card" key={`${flight_number}${index}`}>
+                    <img
+                      src={links.mission_patch_small}
+                      alt="Avatar"
+                      style={{ width: "100%" }}
+                    />
+                    <div className="container">
+                      <h4>
+                        <strong>{mission_name}</strong> #{" "}
+                        <strong>{flight_number}</strong>
+                      </h4>
+                      <h5>MissionIds :{mission_id}</h5>
+                      <h5>Launch Year: {launch_year}</h5>
+                      <h5>
+                        Successful Lunch:{" "}
+                        {launch_success === true ? "true" : "false"}
+                      </h5>
+                      <h5>
+                        Successful Landing:{" "}
+                        {first_stage.cores[0].land_success === true
+                          ? "true"
+                          : "false"}
+                      </h5>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
           </div>
         </div>
       </div>
